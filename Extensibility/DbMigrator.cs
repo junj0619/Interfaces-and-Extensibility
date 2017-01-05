c1136958d8f23038a00d5b70d8c4a2aeaab670a9
Jun <Jun@JUN-PC>
2017-01-05 00:00:04 -0500

    Interfaces and Extensibility

---
+++ Extensibility/DbMigrator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Extensibility
+{
+    //Open Closed Principle [OCP]
+    //Software entities should be open for extension but closed for modification.
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+
+        public DbMigrator(ILogger logger)//Dependencies Injection
+        {
+            _logger = logger;
+        }   
+
+        public void Migrate()
+        {
+            _logger.LogInfo("Migrating started at:" + DateTime.Now);
+
+            //Details of migrating the database
+
+            _logger.LogInfo("Migrating finished at:" + DateTime.Now.AddMinutes(1));
+        }
+    }
+}
---
+++ Extensibility/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var dbMigrator = new DbMigrator(new ConsoleLogger());
+            var dbMigrator = new DbMigrator(new FileLogger("D:\\log.txt"));
+            dbMigrator.Migrate();
+        }
+    }
+}
